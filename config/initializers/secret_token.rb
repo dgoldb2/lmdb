a4b32124005cf5e65aef88166523f3a2047272d2
David J. Treering <dtreering@gmail.com>
2012-07-25 16:55:07 -0500

    initial commit of lmdb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lmdb::Application.config.secret_token = '6794ac694002d1f2fe69b3232d4e91e46fc41873dec81210d6c6d223de9bc171f432a34e3293d487ae61a549ac88894ee8588e8663f1285aca3cf391074db4ea'
